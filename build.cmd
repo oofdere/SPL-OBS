259acc01df128fad94bc583aeafeab78a2cfd73b
oofdere <43102807+oofdere@users.noreply.github.com>
2020-08-10 19:36:03 -0700

    update build process
     - change OBS-SPL to SPL-OBS in build.cmd
     - add metadata to executables

--- build.cmd
+++ build.cmd
@@ -1,8 +1,9 @@
-pyinstaller --onefile --name "OBS-SPL Updater" script.py
-pyinstaller --onefile --name "OBS-SPL Countdown" countdown.py
+pyinstaller --onefile --name "SPL-OBS Updater" script.py --version-file "winupdaterinfo.txt"
+pyinstaller --onefile --name "SPL-OBS Countdown" countdown.py --version-file "wincountdowninfo.txt"
 copy input.yml dist
 copy duration.txt dist
 copy README.md dist
 copy schedule.txt dist
 copy spls-template.yml dist
 copy install.cmd dist
+copy LICNESE dist
